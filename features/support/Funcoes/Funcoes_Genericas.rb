1321dd3e78eaaffb3ed129262b26ab4ff5702738
João Tadeu <ojoaotadeu@gmail.com>
2024-02-24 18:36:27 -0300

    atualizações do projeto de cadasto de parceiro

--- features/support/Funcoes/Funcoes_Genericas.rb
+++ features/support/Funcoes/Funcoes_Genericas.rb
@@ -1,6 +1,6 @@
 class Funoes_Genericas
 
-def self.countdown(seconds)
+  def self.countdown(seconds)
     seconds.times do |i|
       print "\r-> WARN: Aguardando #{seconds - i} Segundos"
       sleep(1)
