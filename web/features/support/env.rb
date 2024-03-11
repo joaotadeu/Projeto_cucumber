9a0b7f1ac7bd2fd9384ea5150db77b0363e69341
João Tadeu <ojoaotadeu@gmail.com>
2024-03-11 18:58:43 -0300

    adicionando melhorias

---
+++ web/features/support/env.rb
@@ -0,0 +1,15 @@
+require "cucumber"
+require "capybara/cucumber"
+require "site_prism"
+require 'report_builder'
+#require 'allure-cucumber'
+
+Capybara.configure do |config|
+    config.default_driver = :selenium
+    config.default_max_wait_time = 5
+end
+
+# AllureCucumber.configure do |config|
+#     config.results_directory = "/logs"
+#     config.clear_results_directory = true
+# end
