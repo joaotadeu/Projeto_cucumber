dccf37c902d7aed376aa88a5b5ce8c71fd52e513
João Tadeu <ojoaotadeu@gmail.com>
2024-03-20 16:09:33 -0300

    adicionando melhorias ao projeto

--- web/features/support/libs/mongo.rb
+++ web/features/support/libs/mongo.rb
@@ -3,15 +3,15 @@ require 'mongo'
 Mongo::Logger.logger = Logger.new("/Users/joaotadeu/Documents/Workspace/Cucumber/web/log/mongo.log")
 
 class MongoDB
-  def Remove_user(email)
-    begin
-      client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
-      users = client[:users]
-      users.delete_many(email: email)
-    rescue StandardError => e
-      puts "Erro ao remover usuário: #{e.message}"
-    ensure
-      client.close if client
+    def Remove_user(email)
+        begin
+            client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+            users = client[:users]
+            users.delete_many(email: email)
+        rescue StandardError => e
+            puts "Erro ao remover usuário: #{e.message}"
+        ensure
+            client.close if client
+        end
     end
-  end
 end
